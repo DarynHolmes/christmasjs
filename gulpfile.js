a9b3dec05690d40579c30f21e5840be638d2db62
Daryn Holmes <darynholmes@gmail.com>
2016-11-23 23:47:49 +0000

    initial commit

---
+++ gulpfile.js
@@ -0,0 +1,51 @@
+const gulp = require('gulp');
+const del = require('del');
+const babel = require('gulp-babel');
+const run_sequence = require('run-sequence');
+const connect = require('gulp-connect');
+const watch = require('gulp-watch');
+const mocha = require('gulp-mocha');
+
+gulp.task('clean', function(done) {
+  return del(['./build'], done);
+});
+
+gulp.task('markup', function() {
+  gulp.src('./src/*.html')
+      .pipe(gulp.dest('./build/'));
+});
+
+gulp.task('compile', function() {
+  return gulp.src('src/script/**/*.js')
+              .pipe(babel({
+                  presets: ['es2015']
+              }))
+             .pipe(gulp.dest('./build/js'));
+});
+
+gulp.task('styles', function() {
+  return gulp.src('./src/styles/**/*.css')
+             .pipe(gulp.dest('./build/css/'));
+});
+
+gulp.task('build', function(done) {
+  run_sequence('clean', ['markup', 'compile', 'styles', 'test'], done);
+});
+
+gulp.task('test', () =>
+    gulp.src('test/**/*.js', {read: false})
+        // gulp-mocha needs filepaths so you can't have any plugins before it
+        .pipe(mocha({reporter: 'nyan'}))
+);
+
+gulp.task('watch', function() {
+     gulp.watch(["./src/**/*.*", "./test/**/*.*"], ['test']);
+});
+
+gulp.task('serve', ['build'], function() {
+  connect.server({livereload: true, root: './build'});
+  gulp.watch(["./src/**/*.*", "./test/**/*.*"], ['build']);
+  watch("./build/**/*.*").pipe(connect.reload());
+});
+
+gulp.task('default', ['serve']);
---
+++ src/script/main.js
@@ -0,0 +1,20 @@
+[1, 2, 3].map(x => x * 2).forEach(i => console.log(i));
+
+const a = 100;
+
+console.log('a', a);
+
+class ClassName {
+    constructor() {
+
+    }
+
+    show() {
+        console.log('Object', 100);
+    }
+}
+
+
+const x = new ClassName();
+
+x.show();
